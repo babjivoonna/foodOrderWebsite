065f9ed671e0fad88b26c0dfa357454625787190
babjivoonna <babjivoonna189@gmail.com>
2024-04-18 15:42:09 +0530

    complete frontend

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum neque repudiandae ex?</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
